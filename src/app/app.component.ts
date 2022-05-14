2e436e40909769fb32dcdfd3f3cc047166540e29
Seborider <mayer.sebobo@gmail.com>
2022-05-14 15:35:02 +0200

    ✨ Add data to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://unsplash.com/photos/KMn4VEeEPR8'
+    },
+
+    {
+      title: 'Mountain with nightsky',
+      url: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title: 'Forrest from above',
+      url: 'https://images.unsplash.com/photo-1558022103-603c34ab10ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9ycmVzdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title: 'Dune in the desert',
+      url: 'https://images.unsplash.com/photo-1511860810434-a92f84c6f01e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVzZXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+
+  ]
 }
